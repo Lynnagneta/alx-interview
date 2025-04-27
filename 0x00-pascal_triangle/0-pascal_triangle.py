a88b8f341fa1ff5601468a8c3f36896f5e1708cc
Lynnagneta <lynnetteagneta@live.com>
2025-04-27 20:54:44 +0000

    pascal trainagle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" this is a pascal triangle generating file"""
+
+
+def pascal_triangle(n):
+    """
+    prints lists of integers representing the Pascal’s triangle of n
+    """
+    res = []
+    if n > 0:
+        """print only if n is greater than 0"""
+        for i in range(1, n + 1):
+            new_list = []
+            C = 1
+            for j in range(1, i + 1):
+                new_list.append(C)
+                C = C * (i - j) // j
+                res.append(new_list)
+    return res
