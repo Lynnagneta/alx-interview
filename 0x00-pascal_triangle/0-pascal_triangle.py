8a4235c61cad5359dab3bfd2c4262316828319e3
Lynnagneta <lynnetteagneta@live.com>
2025-04-27 21:02:40 +0000

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,12 +8,12 @@ def pascal_triangle(n):
     """
     res = []
     if n > 0:
-        """print only if n is greater than 0"""
+        """executed only when n is greater than 0"""
         for i in range(1, n + 1):
             new_list = []
             C = 1
             for j in range(1, i + 1):
                 new_list.append(C)
                 C = C * (i - j) // j
-                res.append(new_list)
+            res.append(new_list)
     return res
